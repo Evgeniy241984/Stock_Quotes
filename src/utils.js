f1c8b169dafe71f6a92bef5e7915e01022294cf0
Evgeniy241984 <evgeniy241984@gmail.com>
2021-07-02 16:00:15 +0300

    initial state and hooks

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,5 @@ export default allowedTickers;
 export function getDateFromUnixTimestamp(dt) {
   return new Date(dt * 1000).toLocaleDateString();
 }
+
+export const isFunction = func => typeof func === 'function';
